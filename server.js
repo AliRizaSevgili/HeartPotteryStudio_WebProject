8d1eb77fa80bc152da103887fe54cebb3b267360
Ali Riza Sevgili <alirizasevgili@outlook.com>
2025-03-06 15:26:26 -0500

    view file was uptaded and added new. hbs files

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.use(
   })
 );
 
-
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/css", express.static(path.join(__dirname, "public/css")));
 app.use("/js", express.static(path.join(__dirname, "public/js")));
@@ -61,21 +60,71 @@ connectDB();
 console.log("✅ Server loaded galleryRoutes!");
 app.use("/api/gallery", galleryRoutes);
 
-
 app.get("/favicon.ico", (req, res) => {
   res.redirect("https://res.cloudinary.com/dnemf1asq/image/upload/v1738886856/132204918_312399920214874_1423945434468733240_n_jf9vhp.ico");
 });
 
-
+// Main Pages Routes
 app.get("/", (req, res) => {
-  res.render("homepage", { layout: "layouts/main", title: "Heart Pottery Studio" });
+  res.render("homepage", {
+    layout: "layouts/main", 
+    title: "Heart Pottery Studio",
+    activeHome: true
+   });
 });
 
+app.get("/about", (req, res) => {
+  res.render("about", { 
+    layout: "layouts/main", 
+    title: "About",
+    activeAbout: true
+  });
+  
+});
 
+app.get("/gallery", (req, res) => {
+  res.render("gallery", { 
+    layout: "layouts/main", 
+    title: "Art Gallery",
+    activeGallery: true
+  });
+  
+});
+
+app.get("/learn", (req, res) => {
+  res.render("learn", { 
+    layout: "layouts/main", 
+    title: "Learn Pottery",
+    activeLearn: true
+  });
+  
+});
+
+app.get("/join", (req, res) => {
+  res.render("studio", { 
+    layout: "layouts/main", 
+    title: "Join the Studio", 
+    activeJoin: true
+  });
+  
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { 
+  layout: "layouts/main", 
+  title: "Get in Touch",
+  activeContact: true
+   });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login", { layout: "layouts/main", title: "Sign In" });
+});
+
+// Handle 404 errors
 app.use((req, res) => {
   res.status(404).json({ error: "Not Found" });
 });
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server is running on port ${PORT}`));
