bad0715c1539a17d9f8c3963fd63efca326a12ec
Ali Riza Sevgili <alirizasevgili@outlook.com>
2025-03-06 21:14:51 -0500

    Added sticky footer and adjusted layout structure

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,17 +1,23 @@
-
-
-document.addEventListener("scroll", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const footer = document.getElementById("hidden-footer");
-  const scrollPosition = window.scrollY + window.innerHeight;
-  const documentHeight = document.documentElement.scrollHeight;
-
-  if (scrollPosition >= documentHeight - 10) {
-      
-      footer.classList.remove("opacity-0");
-      footer.classList.add("opacity-100");
-  } else {
-      
-      footer.classList.remove("opacity-100");
-      footer.classList.add("opacity-0");
+  
+  if (!footer) {
+      console.error("Footer elementi bulunamadı.");
+      return;
   }
+  
+  window.addEventListener("scroll", function () {
+      const scrollPosition = window.scrollY + window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      
+      if (scrollPosition >= documentHeight - 10) {
+          
+          footer.classList.remove("opacity-0");
+          footer.classList.add("opacity-100");
+      } else {
+          
+          footer.classList.remove("opacity-100");
+          footer.classList.add("opacity-0");
+      }
+  });
 });
